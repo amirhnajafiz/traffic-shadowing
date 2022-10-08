10b10f8ef84a857b6c590e2e84780bb7edaa6c9c
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-08 15:52:37 +0330

    add: install

--- setup/install.sh
+++ setup/install.sh
@@ -2,3 +2,20 @@
 
 # installing gor on your local system
 # for macOS
+curl https://github.com/buger/goreplay/archive/refs/tags/v2.0.0-rc2.tar.gz -O -J -L
+
+# unzip the downloaded file
+tar -xf goreplay-2.0.0-rc2.tar.gz
+
+# remove zip file
+rm -rf goreplay-2.0.0-rc2.tar.gz
+
+# make directory
+mkdir /usr/local/bin/gor
+
+# shellcheck disable=SC2164
+cd goreplay-2.0.0-rc2
+
+# move the files to bin
+# shellcheck disable=SC2035
+mv * /usr/local/bin/gor
