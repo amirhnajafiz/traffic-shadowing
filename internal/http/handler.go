260a6ff48f078664de30bf04342e2ca3b58c8cb0
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-09 09:36:19 +0330

    add: handler and methods

--- internal/http/handler.go
+++ internal/http/handler.go
@@ -1,3 +1,16 @@
 package http
 
-// todo: declare http handler
+import (
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Handler struct{}
+
+func (h *Handler) HandleGetRequests(ctx echo.Context) error {
+	log.Printf("user %s", ctx.RealIP())
+
+	return ctx.NoContent(http.StatusNoContent)
+}
