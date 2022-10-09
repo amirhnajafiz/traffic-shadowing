ba538b790445fdaeca1eb815a582ccb84e942e92
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-09 09:37:37 +0330

    feat: main and echo server

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"log"
+
+	"github.com/amirhnajafiz/traffic-shadowing/internal/http"
+	"github.com/labstack/echo/v4"
+)
+
 func main() {
-	// todo: execute in here
+	h := http.Handler{}
+	app := echo.New()
+
+	app.GET("/", h.HandleGetRequests)
+
+	log.Println(app.Start(":8080"))
 }
