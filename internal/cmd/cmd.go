d6df38e1fa4d45c1c8f4bac361a6cac9f8244f24
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-09 09:48:20 +0330

    update: cmd main method

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -9,7 +9,10 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+// main function for starting
+// a http service with echo framework.
 func main() {
+	// getting service port from command line arguments
 	var port string
 
 	if len(os.Args) > 1 {
@@ -18,6 +21,7 @@ func main() {
 		panic(fmt.Errorf("must give the port argument"))
 	}
 
+	// exporting service logs into a logs[port].txt
 	f, err := os.OpenFile("logs"+os.Args[1]+".txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		panic(fmt.Errorf("creating log file failed: %w", err))
@@ -25,10 +29,12 @@ func main() {
 
 	log.SetOutput(f)
 
+	// creating a new handler
 	h := http.Handler{}
+	// creating a new echo app
 	app := echo.New()
-
+	// set the default handler
 	app.GET("/", h.HandleGetRequests)
-
+	// starting the application
 	log.Println(app.Start(port))
 }
