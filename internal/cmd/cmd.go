5bd18515737f3bae7164100a36bd8de57576d781
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-09 09:39:17 +0330

    add: port reading from cmd

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/amirhnajafiz/traffic-shadowing/internal/http"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	var port string
+
+	if len(os.Args) > 1 {
+		port = ":" + os.Args[1]
+	} else {
+		panic(fmt.Errorf("must give the port argument"))
+	}
+
 	h := http.Handler{}
 	app := echo.New()
 
 	app.GET("/", h.HandleGetRequests)
 
-	log.Println(app.Start(":8080"))
+	log.Println(app.Start(port))
 }
